addb4544fb87190cc279e10daa59cee5550e0ef8
Paul Huntsberger <paul@303software.com>
2014-06-16 12:54:55 -0600

    Adding Module files

---
+++ templates/form-admin.tpl.php
@@ -0,0 +1,8 @@
+<h2>Manage Your Add This Installation</h2>
+<p>Add this has simplified how widgets are added to your site. One can enable side-bars through your add this account.<p>
+<p>Sharing inline buttons, is also very simple and you activate that from your account. Since you control your layout,
+this module intentionally allows you to put your content where you want it.</p>
+<p>Want buttons in a block? Put it in a block! Want it in your template, put it there! Want it in a panel? Make one!</p>
+<?php foreach ($form as $element): ?>
+  <?php print drupal_render($element); ?>
+<?php endforeach; ?>
